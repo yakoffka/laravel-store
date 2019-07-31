652f14f5061159afd8336bb9be7ad3f0c7a3e1ee
yo <yagithub@mail.ru>
2019-08-01 02:37:41 +0300

    adding config option ('custom.products_seed')

--- config/custom.php
+++ config/custom.php
@@ -3,4 +3,5 @@
     return [
         'products_paginate' => env('PRODUCTS_PAGINATE', 6),
         'orders_paginate' => env('ORDERS_PAGINATE', 20),
+        'products_seed' => env('products_seed', 100),
     ];
