c8241202a71a8cbe9f8f2c481a1840e6a7bc63a2
yo <yagithub@mail.ru>
2019-08-04 17:54:04 +0300

    correction of the numbering of the settings list

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -13,27 +13,43 @@
     @if ($groups->count())
         <h4>группы настроек</h4>
         <ol>
-            @foreach ($groups as $j => $group)
+            {{-- {{dd(count($groups))}} --}}
+            @foreach ($groups as $group)
                 <li class=""><a href="#{{ $group }}">{{ $group }}</a>
             @endforeach
         </ol>
     @endif
 
-    @foreach ($groups as $i => $group)
+    @php
+        $j = 1;
+    @endphp
+
+    @foreach ($groups as $group)
         <div class="" id="{{ $group }}"></div>
         <div class="card setting_group">
-            <div class="card-header"><h3>{{ ($i +1) }} Настройки группы '{{ $group }}'</h3></div>
-            @foreach ($settings as $j => $setting)
+
+            @php
+                if (!empty($i)) {
+                    $j++;
+                }
+                $i = 0;
+            @endphp
+
+            <div class="card-header"><h3>{{ $j }} Настройки группы '{{ $group }}'</h3></div>
+
+            @foreach ($settings as $setting)
+            
                 @if ($setting->group == $group)
 
                     @php
+                        $i++;
                         $permissible_values = unserialize($setting->permissible_values);
                     @endphp
 
                     <div class="card-body">
-                        <h5 class="card-title">{{ ($i + 1) . '.' . ($j + 1) }} {{ $setting->display_name }}</h5>
+                        <h5 class="card-title">{{ $j . '.' . $i }} {{ $setting->display_name }}</h5>
                         <p class="card-text">
-                            {{ $setting->description }} ({{ $setting->description }})
+                            {{ $setting->description }}{{--  ({{ $setting->value }}) --}}
                         </p>
                         <form action="{{ route('settings.update', ['setting' => $setting->id]) }}" method="POST">
                             @csrf
@@ -60,7 +76,9 @@
                             <button type="submit"  class="btn btn-primary">применить</button>
                         </form>
                     </div>
+
                 @endif
+
             @endforeach
         </div>
     @endforeach
