6ae2cab7f159ee5c44614b6450cdb11c9f38654e
yo <yagithub@mail.ru>
2019-08-18 13:33:11 +0300

    building subcategories in the correct order

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -2,10 +2,12 @@
 
 use App\Category;
 
+
 // Catalog
 Breadcrumbs::for('catalog', function ($trail) {
     $trail->push('Catalog', route('products.index'));
 });
+
 
 // Catalog > [Categories]
 Breadcrumbs::for('categories', function ($trail, $category) {
@@ -13,14 +15,23 @@ Breadcrumbs::for('categories', function ($trail, $category) {
     $trail->push($category->title, route('categories.show', $category->id));
 });
 
+
 // Catalog > [Categories] > [Product]
 Breadcrumbs::for('product', function ($trail, $product) {
     $trail->parent('catalog');
 
+    // get all parents:
+    $arr_parents = [];
     $parent = $product->category;
     while ( $parent->id > 1 ) {
-        $trail->push($parent->title, route('categories.show', $parent->id));
+        $arr_parents[] = $parent;
         $parent = Category::find($parent->parent_id);
+    }
+    $arr_parents = array_reverse($arr_parents);
+
+    // get breadcrumbs for all parents:
+    foreach ( $arr_parents as $parent ) {
+        $trail->push($parent->title, route('categories.show', $parent->id));
     }
 
     $trail->push($product->name, route('products.show', ['product' => $product->id]));
