b39234dd1e2ec3610d7fd8d05bd4a481d24dc214
vagrant <yagithub@mail.ru>
2019-11-08 18:57:58 +0000

    Prevention of duplication of mail during seeding.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -27,8 +27,8 @@ class UsersTableSeeder extends Seeder
             ['name' => 'Cmanager'.config('custom.name_owner'),      'email' => str_replace('@', '+cmanager@', config('custom.mail_owner')),     'passw' => config('custom.pass_owner'), ], 
             ['name' => 'Smanager'.config('custom.name_owner'),      'email' => str_replace('@', '+smanager@', config('custom.mail_owner')),     'passw' => config('custom.pass_owner'), ], 
             ['name' => 'Unregistered',                              'email' => str_replace('@', '+unregistered@', config('mail.from.address')), 'passw' => config('custom.pass_owner'), ], 
-            ['name' => 'User1'.config('custom.name_owner'),         'email' => str_replace('@', '+user1@', config('custom.mail_owner')),        'passw' => config('custom.pass_owner'), ], 
-            ['name' => 'User1'.config('custom.name_devel'),         'email' => str_replace('@', '+user1@', config('custom.mail_devel')),        'passw' => config('custom.pass_devel'), ], 
+            ['name' => 'User1'.config('custom.name_owner'),         'email' => str_replace('@', '+user1o@', config('custom.mail_owner')),        'passw' => config('custom.pass_owner'), ], 
+            ['name' => 'User1'.config('custom.name_devel'),         'email' => str_replace('@', '+user1d@', config('custom.mail_devel')),        'passw' => config('custom.pass_devel'), ], 
         ];
 
         foreach ( $users as $user ) {
