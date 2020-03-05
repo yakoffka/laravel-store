feb7b89fc65b0086318fe3c209f073096422a9e0
yakoffka <yagithub@mail.ru>
2020-03-05 22:01:43 +0400

    Refactoring: adding CategoryRequest     adding CategoryRequest;

---
+++ app/Http/Requests/CategoryRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+//        return false;
+        if ( isset( $this->category ) ) {
+            return auth()->user()->can('edit_categories');
+        }
+
+        return auth()->user()->can('create_categories');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'name'          => [
+                'required',
+                'string',
+                'max:255',
+                isset( $this->category ) ? 'unique:categories,name,'.$this->category->id.',id' : 'unique:categories,name',
+            ],
+            'title'         => 'nullable|string|max:255',
+            'slug'          => 'nullable|string|max:255',
+            'description'   => 'nullable|string|max:65535',
+            'imagepath'     => 'nullable|string|max:255',
+            'parent_id'     => 'required|integer|max:255',
+            'sort_order'    => 'required|string|max:1',
+            'seeable'       => 'nullable|string|in:on',
+        ];
+    }
+}
