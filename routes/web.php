0a12a18757713f90eb3a122ff2420b9809669def
yakoffka <yagithub@mail.ru>
2020-03-08 02:17:38 +0400

    Refactoring routes

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,6 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-// Route::get('/', function () {
-//     return redirect( route('home') );
-// });
-// Route::get('/', function () {
-//     return redirect( route('mainpage') );
-// });
 
 /*
     GET '/items' ItemController@index
@@ -29,10 +20,6 @@
     GET '/items/edit/{id}' ItemController@edit
     PATCH '/items/{id}' ItemController@update
     DELETE '/items/{id}' ItemController@destroy
-
-    Regular Expression Constraints && Generating URLs To Named Routes:
-    Route::get('/items/{id}', 'ItemController@show')->where(['id' => '[0-9]+'])->name('itemsShow');
-    $url = route('itemsShow', ['id' => 1]);
 */
 
 
@@ -42,96 +29,55 @@ if ( !empty(config('settings.display_registration')) ) {
     Auth::routes(['register' => false]);
 }
 
+/* main */
+    Route::get('/', 'HomeController@home')->name('home');
+    Route::get('/dashboard', 'HomeController@dashboard')->name('dashboard');
+    Route::get('/verify/{token}', 'Auth\RegisterController@verify')->name('register.verify');
+    Route::post('/contact_us', 'HomeController@contactUs')->name('home.contact_us');
 
-Route::get('/', 'HomeController@home')->name('home');
-Route::get('/dashboard', 'HomeController@dashboard')->name('dashboard');
-Route::get('/verify/{token}', 'Auth\RegisterController@verify')->name('register.verify');
-
-Route::post('/contact_us', 'HomeController@contactUs')->name('home.contact_us');
-
-
-
-/* products*/
-    Route::get      ('/products',               'ProductsController@index'      )->name('products.index');
-    Route::get      ('/products/create',        'ProductsController@create'     )->name('products.create');
-    Route::get      ('/products/{product}',     'ProductsController@show'       )->name('products.show');
-    Route::post     ('/products',               'ProductsController@store'      )->name('products.store');
-    Route::get      ('/products/{product}/edit','ProductsController@edit'       )->name('products.edit');
-    Route::patch    ('/products/{product}',     'ProductsController@update'     )->name('products.update');
-    Route::delete   ('/products/{product}',     'ProductsController@destroy'    )->name('products.destroy');
-    // filter
-    // Route::get('products.filter', 'ProductsController@filter')->name('products.filter');
-    // rewatermark
+/* products */
+    Route::resource ('/products',               'ProductsController');
     Route::get      ('admin/products/rewatermark',   'ProductsController@rewatermark')->name('products.rewatermark')->middleware('auth');
     Route::get      ('admin/products/{product}/copy','ProductsController@copy'       )->name('products.copy')->middleware('auth');
+    Route::get      ('admin/products',               'ProductsController@adminIndex' )->name('products.adminindex'  )->middleware('auth');
+    Route::get      ('admin/products/{product}',     'ProductsController@adminShow'  )->name('products.adminshow'   )->middleware('auth');
+    Route::post     ('admin/products/massupdate',    'ProductsController@massupdate' )->name('products.massupdate'  )->middleware('auth');
+    Route::get      ('search',                       'ProductsController@search'     )->name('search');
 
-    Route::get      ('admin/products',          'ProductsController@adminIndex' )->name('products.adminindex'  )->middleware('auth');
-    Route::get      ('admin/products/{product}','ProductsController@adminShow'  )->name('products.adminshow'   )->middleware('auth');
-    // Route::get      ('admin/products/{product}','ProductsController@show'       )->name('products.adminshow'   )->middleware('auth');
-    Route::post     ('admin/products/massupdate', 'ProductsController@massupdate')->name('products.massupdate'  )->middleware('auth');
+/* comments */
+    Route::post('/products/{product}/comments', 'ProductCommentsController@store');
+    Route::patch('/comments/{comment}', 'ProductCommentsController@update');
+    Route::delete('/comments/{comment}', 'ProductCommentsController@destroy')->name('comments.destroy');
 
+/* users */
+    Route::resource('/users', 'UsersController')->except(['store', 'create']);
+    Route::patch('/users/{user}/password-change', 'UsersController@passwordChange')->name('users.password-change');
 
-/* comments*/
-//Route::get('/comments', 'ProductCommentsController@index');
-//Route::get('/comments/create', 'ProductCommentsController@create');
-//Route::get('/comments/{comment}', 'ProductCommentsController@show');
-Route::post('/products/{product}/comments', 'ProductCommentsController@store');
-//Route::get('/comments/edit/{comment}', 'ProductCommentsController@edit');
-Route::patch('/comments/{comment}', 'ProductCommentsController@update');
-Route::delete('/comments/{comment}', 'ProductCommentsController@destroy')->name('comments.destroy');
+/* roles */
+    Route::resource('/roles', 'RolesController');
 
-/* users*/
-Route::get('/users', 'UsersController@index')->name('users.index');
-// Route::get('/users/create', 'UsersController@create')->name('users.create');
-Route::get('/users/{user}', 'UsersController@show')->name('users.show');
-// Route::post('/users', 'UsersController@store')->name('users.store');
-Route::get('/users/edit/{user}', 'UsersController@edit')->name('users.edit');
-Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
-Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
-Route::patch('/users/{user}/password-change', 'UsersController@passwordChange')->name('users.password-change');
-
-/* roles*/
-Route::get('/roles', 'RolesController@index')->name('roles.index');
-Route::get('/roles/create', 'RolesController@create')->name('roles.create');
-Route::get('/roles/{role}', 'RolesController@show')->name('roles.show');
-Route::post('/roles', 'RolesController@store')->name('roles.store');
-Route::get('/roles/edit/{role}', 'RolesController@edit')->name('roles.edit');
-Route::patch('/roles/{role}', 'RolesController@update')->name('roles.update');
-Route::delete('/roles/{role}', 'RolesController@destroy')->name('roles.destroy');
-
-
-/* categories*/
-    // POST       categories                 categories.store    CategoryController@store    web,auth                                                                                                               |
-    // GET|HEAD   categories                 categories.index    CategoryController@index    web                                                                                                                    |
-    // GET|HEAD   categories/create          categories.create   CategoryController@create   web,auth                                                                                                               |
-    // DELETE     categories/{category}      categories.destroy  CategoryController@destroy  web,auth                                                                                                               |
-    // GET|HEAD   categories/{category}      categories.show     CategoryController@show     web                                                                                                                    |
-    // PUT|PATCH  categories/{category}      categories.update   CategoryController@update   web,auth                                                                                                               |
-    // GET|HEAD   categories/{category}/edit categories.edit     CategoryController@edit     web,auth                                                                                                               |
+/* categories */
     Route::resource('categories', 'CategoryController');
-    Route::get   ('admin/categories',                       'CategoryController@adminIndex')->name('categories.adminindex'  )->middleware('auth');
-    Route::get   ('admin/categories/{category}',            'CategoryController@adminShow' )->name('categories.adminshow'   )->middleware('auth');
+    Route::get('admin/categories',            'CategoryController@adminIndex')->name('categories.adminindex')->middleware('auth');
+    Route::get('admin/categories/{category}', 'CategoryController@adminShow' )->name('categories.adminshow' )->middleware('auth');
 
-// cart
-Route::get('cart/add/{product}', 'CartController@addToCart')->name('cart.add-item');
-Route::get('cart', 'CartController@show')->name('cart.show');
-Route::get('cart/confirmation', 'CartController@confirmation')->name('cart.confirmation')->middleware('auth');
-Route::patch('cart/change/{product}', 'CartController@changeItem')->name('cart.change-item');
-Route::delete('cart/delete/{product}', 'CartController@deleteItem')->name('cart.delete-item');
+/* cart */
+    Route::get('cart/add/{product}','CartController@addToCart')->name('cart.add-item');
+    Route::get('cart',              'CartController@show')->name('cart.show');
+    Route::get('cart/confirmation', 'CartController@confirmation')->name('cart.confirmation')->middleware('auth');
+    Route::patch('cart/change/{product}','CartController@changeItem')->name('cart.change-item');
+    Route::delete('cart/delete/{product}','CartController@deleteItem')->name('cart.delete-item');
 
 // orders
-    Route::resource('orders',      'OrderController')->except(['edit']);
-    Route::get('admin/orders',          'OrderController@adminIndex')->name('orders.adminindex'  )->middleware('auth');
-    Route::get('admin/orders/{order}',  'OrderController@adminShow' )->name('orders.adminshow'   )->middleware('auth');
-
+    Route::resource('orders',    'OrderController')->except(['edit']);
+    Route::get('admin/orders',        'OrderController@adminIndex')->name('orders.adminindex'  )->middleware('auth');
+    Route::get('admin/orders/{order}','OrderController@adminShow' )->name('orders.adminshow'   )->middleware('auth');
 
 // images
-Route::delete('images/{image}', 'ImagesController@destroy')->name('images.destroy');
-Route::patch('images/{image}', 'ImagesController@update')->name('images.update');
+    Route::delete('images/{image}','ImagesController@destroy')->name('images.destroy');
+    Route::patch('images/{image}', 'ImagesController@update')->name('images.update');
 
 
-// search
-Route::get('search', 'ProductsController@search')->name('search');
 
 
 
@@ -142,15 +88,12 @@ Route::get('search', 'ProductsController@search')->name('search');
     // settings
     Route::resource('settings', 'SettingController');
 
-
     // customevents
-        // all
         Route::get('customevents', 'CustomeventController@index')->name('customevents.index');
         Route::get('customevents/{customevent}', 'CustomeventController@show')->name('customevents.show');
 
     // manufacturers
         Route::resource('manufacturers', 'ManufacturerController')->middleware('auth');
-
 
     // Tasks and Directive
         // tasks
