da6ed0c718498b8c9dd5a51798bd6947feefef49
yakoffka <yagithub@mail.ru>
2020-03-11 10:24:38 +0400

    Adding a note

--- tests/Feature/ResponseTest.php
+++ tests/Feature/ResponseTest.php
@@ -65,20 +65,21 @@ class ResponseTest extends TestCase
             '/cart/add/' . $product->id,
         ];
 
-//        echo "\nReport from " . __CLASS__ . "\ni: Для успешного прохождения необходимо наличие скрытых директорий и товаров\n";
+        // echo "\nReport from " . __CLASS__ . "\ni: Для успешного прохождения необходимо наличие скрытых директорий и товаров\n";
+        // accessories -> unpublished; drums -> unpublished.
 
         foreach ($getRequests200 as $route) {
             $response = $this->get($route);
-//            echo '    GET \'' . $route . '\': ';
+            // echo '    GET \'' . $route . '\': ';
             $response->assertStatus(200);
-//            echo "200 - OK!\n";
+            // echo "200 - OK!\n";
         }
 
         foreach ($getRequests302 as $route) {
             $response = $this->get($route);
-//            echo '    GET \'' . $route . '\': ';
+            // echo '    GET \'' . $route . '\': ';
             $response->assertStatus(302);
-//            echo "302 - OK!\n";
+            // echo "302 - OK!\n";
         }
     }
 
@@ -140,9 +141,9 @@ class ResponseTest extends TestCase
         echo "\n";
         foreach ($getRequests404 as $route) {
             $response = $this->get($route);
-//            echo '    GET \'' . $route . '\': ';
+            // echo '    GET \'' . $route . '\': ';
             $response->assertStatus(404);
-//            echo "404 - OK!\n";
+            // echo "404 - OK!\n";
         }
     }
 }
